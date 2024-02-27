ac782bf6776458a9a61cf44fab1616a3b58efb1a
Katya-Semeniuk <katyasemeniuk@gmail.com>
2024-02-27 18:55:09 +0100

    added set functions  up to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-console.log("hallo");
+function runGame() {}
+function checkAnswer() {}
+function calculateCorrectAnswer() {}
+function incrementScore() {}
+function incrementWrongAnswer() {}
+function displayAdditionQuestion() {}
+function displaySubtractQuestion() {}
+function displayMultiplyQuestion() {}
+function displayDivisionQuestion() {}
