f69ac832da85304269ee0d266a70f93f03f6c734
Katya-Semeniuk <katyasemeniuk@gmail.com>
2024-02-28 14:48:19 +0100

    added code for dividion game

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,8 @@ function runGame(gameType) {
     displayMultiplyQuestion(num1, num2);
   } else if (gameType === "subtract") {
     displaySubtractQuestion(num1, num2);
+  } else if (gameType === "division") {
+    displayDivisionQuestion(num1, num2);
   } else {
     alert(`Unknown type ${gameType}`);
     throw `Unknown type ${gameType}. Aborting!`;
@@ -79,6 +81,10 @@ function calculateCorrectAnswer() {
     return [operand1 * operand2, "multiply"];
   } else if (operator === "-") {
     return [operand1 - operand2, "subtract"];
+  } else if (operator === "/") {
+    let result = Math.floor(operand1 / operand2);
+    console.log(result);
+    return [result, "division"];
   } else {
     alert(`Unimplementing operator ${operator}`);
     throw `Unimplementing operator ${operator}. Aborting!`;
@@ -122,4 +128,13 @@ function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById("operand2").textContent = operand2;
   document.getElementById("operator").textContent = "x";
 }
-function displayDivisionQuestion() {}
+
+function displayDivisionQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent =
+    operand1 > operand2 ? operand1 : operand2;
+
+  document.getElementById("operand2").textContent =
+    operand1 > operand2 ? operand2 : operand1;
+
+  document.getElementById("operator").textContent = "/";
+}
