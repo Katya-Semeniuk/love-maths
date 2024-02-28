159b68b7c92ff8871139b9c9465d8f450b908604
Katya-Semeniuk <katyasemeniuk@gmail.com>
2024-02-28 14:03:54 +0100

    added code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,8 @@ function runGame(gameType) {
     displayAdditionQuestion(num1, num2);
   } else if (gameType === "multiply") {
     displayMultiplyQuestion(num1, num2);
+  } else if (gameType === "subtract") {
+    displaySubtractQuestion(num1, num2);
   } else {
     alert(`Unknown type ${gameType}`);
     throw `Unknown type ${gameType}. Aborting!`;
@@ -65,6 +67,8 @@ function calculateCorrectAnswer() {
     return [operand1 + operand2, "addition"];
   } else if (operator === "x") {
     return [operand1 * operand2, "multiply"];
+  } else if (operator === "-") {
+    return [operand1 - operand2, "subtract"];
   } else {
     alert(`Unimplementing operator ${operator}`);
     throw `Unimplementing operator ${operator}. Aborting!`;
@@ -93,7 +97,15 @@ function displayAdditionQuestion(operand1, operand2) {
   document.getElementById("operator").textContent = "+";
 }
 
-function displaySubtractQuestion() {}
+function displaySubtractQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent =
+    operand1 > operand2 ? operand1 : operand2;
+
+  document.getElementById("operand2").textContent =
+    operand1 > operand2 ? operand2 : operand1;
+
+  document.getElementById("operator").textContent = "-";
+}
 
 function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById("operand1").textContent = operand1;
